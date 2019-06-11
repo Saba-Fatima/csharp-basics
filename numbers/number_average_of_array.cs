cac35e3d4297296725dc53a1c7dc3c38fc538bb7
Saba-Fatima <fatimasaba023@gmail.com>
2019-06-11 21:54:14 +0530

    june 11 commit 2
    Signed-off-by: Saba-Fatima <fatimasaba023@gmail.com>

---
+++ numbers/number_average_of_array.cs
@@ -0,0 +1,18 @@
+#region Average of an array
+			//int[] a = new int[100];
+			//int average=0, n;
+			//Console.WriteLine("Enter length of the array:");
+			//n = Convert.ToInt32(Console.ReadLine());
+			//Console.WriteLine("Enter elements of the array:");
+			//for (int i = 0; i < n; i++)
+			//{
+			//	a[i] = Convert.ToInt32(Console.ReadLine());
+			//}
+			//Console.WriteLine("Average of the array elements:");
+			//for (int i = 0; i < n; i++)
+			//{
+			//	average = (average + a[i]);
+			//}
+			//Console.WriteLine(average / n);
+			//Console.Read();
+			#endregion
