f2c5f39046930bdcb8e300bef01d202ae378a927
Saba-Fatima <fatimasaba023@gmail.com>
2019-06-11 21:52:37 +0530

    june 11 commit
    Signed-off-by: Saba-Fatima <fatimasaba023@gmail.com>

---
+++ numbers/number_sum_of_elements_array.cs
@@ -0,0 +1,20 @@
+#region Sum of the Arrays
+			//int[] a = new int[100];
+			//int i, n, sum = 0;
+			//Console.Write("Input the number of elements to be stored in the array :");
+			//n = Convert.ToInt32(Console.ReadLine());
+
+			//Console.Write("Input {0} elements in the array :\n", n);
+			//for (i = 0; i < n; i++)
+			//{
+			//	a[i] = Convert.ToInt32(Console.ReadLine());
+			//}
+
+			//for (i = 0; i < n; i++)
+			//{
+			//	sum += a[i];
+			//}
+
+			//Console.Write("Sum of all elements stored in the array is : {0}\n\n", sum);
+			//Console.Read();
+			#endregion
