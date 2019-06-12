0d0c26dc58777c171457129a226a62b9144ede73
Saba-Fatima <fatimasaba023@gmail.com>
2019-06-12 22:02:32 +0530

    june 12 commit 2
    Signed-off-by: Saba-Fatima <fatimasaba023@gmail.com>

---
+++ numbers/number_copy_of_array.cs
@@ -0,0 +1,19 @@
+#region copy the elements one array into another array.
+			//int[] a = new int[100];
+			//int[] b = new int[100];
+			//int n;
+			//Console.WriteLine("Enter size of the array:");
+			//n = Convert.ToInt32(Console.ReadLine());
+			//Console.WriteLine("Elements of the array:");
+			//for (int i = 0; i < n; i++)
+			//{
+			//	a[i] = Convert.ToInt32(Console.ReadLine());
+			//}
+			//Console.WriteLine("Elements of the array copied are:");
+			//for (int i = 0; i < n; i++)
+			//{
+			//	b[i] = a[i];
+			//	Console.WriteLine(b[i]);
+			//}
+			//Console.Read();
+			#endregion
