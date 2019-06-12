d77e98d83e9825aeea804a2f34592c5e7dfce6b6
Saba-Fatima <fatimasaba023@gmail.com>
2019-06-12 22:01:09 +0530

    june 12 commit
    Signed-off-by: Saba-Fatima <fatimasaba023@gmail.com>

---
+++ numbers/number_reverse_of_array.cs
@@ -0,0 +1,23 @@
+#region read n number of values in an array and display it in reverse 
+			//int[] a = new int[100];
+			//int reverse = 0, n;
+			//Console.WriteLine("Enter length of the array:");
+			//n = int.Parse(Console.ReadLine());
+			//Console.WriteLine("Enter elements of the array:");
+			//for (int i = 0; i < n; i++)
+			//{
+			//a[i] = int.Parse(Console.ReadLine());
+			//}
+			//Console.WriteLine("elements of the array:");
+			//for (int i = 0; i < n; i++)
+			//{
+			//	Console.WriteLine(a[i]);
+			//}
+			//Console.WriteLine("reverse elements of the array:");
+			//for (int i = n-1; i >= 0 ; i--)
+			//{
+			//	Console.WriteLine(a[i]);
+			//}
+			//Console.WriteLine(reverse);
+			//Console.Read();
+			#endregion
