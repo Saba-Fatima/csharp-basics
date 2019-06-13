f30f4ed5442c49747fd112091f939e23dc09f4a5
Saba-Fatima <fatimasaba023@gmail.com>
2019-06-13 21:28:36 +0530

    june 13 commit
    Signed-off-by: Saba-Fatima <fatimasaba023@gmail.com>

---
+++ numbers/number_duplicate_elements_in_array.cs
@@ -0,0 +1,24 @@
+#region count a total number of duplicate elements in an array
+			//int[] arr1 = new int[100];
+			//int count = 0,n;
+			//Console.WriteLine("Enter size of the array:");
+			//n = Convert.ToInt32(Console.ReadLine());
+			//Console.WriteLine("Add elements to  the array:");
+			//for (int i = 0; i < n; i++)
+			//{
+			//	arr1[i] = Convert.ToInt32(Console.ReadLine());
+			//}
+			//Console.WriteLine("check for duplicate elements in an array:");
+			//for (int i = 0; i < n; i++)
+			//{
+			//	for (int j = 0; j < i; j++)
+			//	{
+			//		if (arr1[i]==arr1[j])
+			//		{
+			//			count++;
+			//		}
+			//	}
+			//}
+			//Console.WriteLine(count);
+			//Console.Read();
+			#endregion
---
+++ numbers/number_unique_elements_in_array.cs
@@ -0,0 +1,28 @@
+#region print all unique elements in an array
+			//int[] arr1 = new int[100];
+			//int n, count = 0;
+			//Console.WriteLine("enter size of the array here:");
+			//n = Convert.ToInt32(Console.ReadLine());
+			//Console.WriteLine("Enter elements of the array here:");
+			//for (int i = 0; i < n; i++)
+			//{
+			//	arr1[i] = Convert.ToInt32(Console.ReadLine());
+			//}
+			//Console.WriteLine("unique elements of the array are:");
+			//for (int i = 0; i < n; i++)
+			//{
+			//	for (int j = 0; j < i; j++)
+			//	{ 
+			//		if (arr1[i] == arr1[j])
+			//		{
+			//			count++;
+			//		}
+			//	}
+			//	if (count == 0)
+			//	{
+			//		Console.Write(arr1[i]);
+			//	}
+			//}
+
+			//Console.Read();
+			#endregion
