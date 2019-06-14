8dff5d9b6058fc14813d5a176e529d750ad6756a
Saba-Fatima <fatimasaba023@gmail.com>
2019-06-14 23:07:42 +0530

    june 14 commit
    Signed-off-by: Saba-Fatima <fatimasaba023@gmail.com>

---
+++ numbers/number_min_max_of_array.cs
@@ -0,0 +1,43 @@
+#region maximum and minimum element in an array.
+			//int[] arr1 = new int[100];
+			//int n,temp=0,temp1=0;
+			//Console.WriteLine("Enter size of the array:");
+			//n = Convert.ToInt32(Console.ReadLine());
+			//Console.WriteLine("Enter elements of the array:");
+			//for (int i = 0; i < n; i++)
+			//{
+			//	arr1[i]= Convert.ToInt32(Console.ReadLine());
+			//}
+			//Console.WriteLine("maximum element of an array:");
+			//for (int i = 0; i < n; i++)
+			//{
+			//	for (int j = 0; j < n-1; j++)
+			//	{
+			//		if (arr1[i]>arr1[j+1])
+			//		{
+			//			temp = arr1[i];
+			//			arr1[j + 1] = arr1[i];
+			//			arr1[i] = temp;
+			//		}
+			//	}
+
+
+			//}
+			//Console.WriteLine("minimum element of an array:");
+			//for (int i = 0; i < n; i++)
+			//{
+			//	for (int k = 0; k < n; k++)
+			//	{
+			//		if (arr1[i] < arr1[k + 1])
+			//		{
+			//			temp1 = arr1[i];
+			//			arr1[k + 1] = arr1[i];
+			//			arr1[i] = temp1;
+			//		}
+			//	}
+			//}
+
+			//Console.WriteLine(temp+""+temp1);
+
+			//Console.Read();
+			#endregion
